06047e325b306878842d136aa40faeee684a53a3
Patrick Damme <patrick.damme@tu-dresden.de>
2020-06-24 20:30:38 +0200

    Minor fix regarding namespaces.

--- vector/type_helper.h
+++ vector/type_helper.h
@@ -1,24 +1,25 @@
 #ifndef MORPHSTORE_VECTOR_TYPE_HELPER_H
 #define MORPHSTORE_VECTOR_TYPE_HELPER_H
 
-using namespace morphstore;
-using namespace vectorlib;
+namespace vectorlib {
 
-template <class S, class T>
-class TypeHelper
-{
-};
+    template <class S, class T>
+    class TypeHelper
+    {
+    };
 
 
-template <template <typename> class T1,
-          template <uint16_t, typename> class T2,
-          uint16_t N,
-          class T3,
-          class T4>
-class TypeHelper<T1<T2<N, T3> >, T4>
-{
-public:
-    typedef T1<T2<N, T4> > newbasetype;
-};
+    template <template <typename> class T1,
+              template <uint16_t, typename> class T2,
+              uint16_t N,
+              class T3,
+              class T4>
+    class TypeHelper<T1<T2<N, T3> >, T4>
+    {
+    public:
+        typedef T1<T2<N, T4> > newbasetype;
+    };
+
+}
 
 #endif
