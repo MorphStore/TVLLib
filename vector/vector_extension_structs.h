42626a1e8eea023cc78b4c3fc3e89711d4646766
Felicita Gernhardt <felicitagernha@gmail.com>
2020-06-24 19:29:43 +0200

    added utility functions

--- vector/vector_extension_structs.h
+++ vector/vector_extension_structs.h
@@ -42,5 +42,17 @@ namespace vectorlib{
    using vector_base_t_granularity MSV_CXX_ATTRIBUTE_PPUNUSED = typename VectorExtension::vector_helper_t::granularity;
 
 
+#define IMPORT_VECTOR_BOILER_PLATE_PREFIX(VectorExtension, Prefix) \
+   using Prefix##vector_element_count MSV_CXX_ATTRIBUTE_PPUNUSED = typename VectorExtension::vector_helper_t::element_count; \
+   using Prefix##base_t MSV_CXX_ATTRIBUTE_PPUNUSED = typename VectorExtension::vector_helper_t::base_t; \
+   using Prefix##vector_size_bit MSV_CXX_ATTRIBUTE_PPUNUSED = typename VectorExtension::vector_helper_t::size_bit; \
+   using Prefix##vector_size_byte MSV_CXX_ATTRIBUTE_PPUNUSED = typename VectorExtension::vector_helper_t::size_byte; \
+   using Prefix##vector_alignment MSV_CXX_ATTRIBUTE_PPUNUSED = typename VectorExtension::vector_helper_t::alignment; \
+   using Prefix##vector_t MSV_CXX_ATTRIBUTE_PPUNUSED = typename VectorExtension::vector_t; \
+   using Prefix##vector_size MSV_CXX_ATTRIBUTE_PPUNUSED = typename VectorExtension::size; \
+   using Prefix##vector_mask_t MSV_CXX_ATTRIBUTE_PPUNUSED = typename VectorExtension::mask_t; \
+   using Prefix##vector_base_t_granularity MSV_CXX_ATTRIBUTE_PPUNUSED = typename VectorExtension::vector_helper_t::granularity;
+
+
 }
 #endif //MORPHSTORE_VECTOR_VECTOR_EXTENSION_STRUCTS_H
